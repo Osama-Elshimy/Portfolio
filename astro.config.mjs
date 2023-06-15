e18b513520a2c7d3473e2d77f6b2cf9120cc7abc
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-06-15 06:54:54 +0300

    Deployed!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+	output: 'server',
+	adapter: netlify(),
 });
