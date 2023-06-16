716a036c9f69181a65a96a3ac1d83861edd99473
Osama-Elshimy <Osama.Elshimy.o.e@gmail.com>
2023-06-16 23:26:53 +0300

    Added PostCSS preset plugin

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,6 +2,6 @@ module.exports = {
 	plugins: [
 		require('autoprefixer'),
 		require('cssnano'),
-		require('postcss-preset-env')({ stage: 1 }),
+		require('postcss-preset-env')({ stage: 2 }),
 	],
 };
